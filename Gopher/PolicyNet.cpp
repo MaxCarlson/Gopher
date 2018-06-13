0c563409ccdfc93980a3e83867fce957d04d7ffc
Maxwell Carlson <bart4282819@gmail.com>
2018-06-12 23:25:40 -0700

    Having some serious issues with tf. Can't seem to get the session to run correctly with what should be the right input name. Also having extern symbol issues with the functions used to get the variable names held in the graph ( was trying to figure out what the inputs actual name was)

--- Gopher/PolicyNet.cpp
+++ Gopher/PolicyNet.cpp
@@ -49,6 +49,23 @@ void init()
 	// Read the weights
 	tf::Tensor checkpointPathTensor(tf::DT_STRING, tf::TensorShape());
 	checkpointPathTensor.scalar<std::string>()() = pathToCheckpoint;
+
+	
+	
+	//auto ss = graphDef.saver_def().filename_tensor_name();
+	//auto sss = graphDef.saver_def().restore_op_name();
+	int sz = graphDef.graph_def().node_size();
+	std::vector<std::string> vNames;
+	for (int i = 0; i < sz; i++) {
+		auto n = graphDef.graph_def().node(i);
+
+		//if (n.name().find("nWeights") != std::string::npos) {
+		//	vNames.push_back(n.name());
+		//}
+	}
+	
+	
+
 	status = session->Run(
 		{ { graphDef.saver_def().filename_tensor_name(), checkpointPathTensor }, },
 		{},
@@ -64,18 +81,42 @@ void init()
 
 void run()
 {
+	static const std::string inputName = "input";
+	static const std::string outputName = "Output";
+
 	auto shape = tf::TensorShape({ BoardDepth, BoardSize, BoardSize});
 
 	int ar[2527];
 
-	auto init = tf::Input::Initializer(*ar, shape);
+	//auto init = tf::Input::Initializer(*ar, shape);
 	//auto placeH = tf::Placeholder
 
+	tf::Tensor input(tf::DT_INT16, shape);
+
+	/*
+	auto inputMap = input.tensor<int, 3>();
+	
+	for (int z = 0; z < BoardDepth; ++z)
+		for (int y = 0; y < BoardSize; ++y)
+			for (int x = 0; x < BoardSize; ++x)
+				inputMap(z, x, y) = ar[z * (y * BoardSize + x)];
+	*/
+
+
+	std::vector<std::pair<std::string, tf::Tensor>> inputs = {
+		{ inputName, input },
+	};
+
+	std::vector<tf::Tensor> outputs;
+
+	status = session->Run(inputs, { outputName }, {}, &outputs);
+
+
+	int a = 5;
 	/*
 	// and run the inference to your liking
 	auto feedDict = ...
 	auto outputOps = ...
-	std::vector<tensorflow::Tensor> outputTensors;
 	status = session->Run(feedDict, outputOps, {}, &outputTensors);
 
 	*/
