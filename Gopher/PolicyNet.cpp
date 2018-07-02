45ccb68969e5b104492b401cafefc1dc0e6f4a38
Maxwell Carlson <bart4282819@gmail.com>
2018-07-02 13:44:42 -0700

    Changed over from Keras/Tensorflow to MIcrosoft CNTK for the machine learning model. Able to successfully load and pass argurments through the model. Will be working on integrating it and modifying board to hold the required input (7 binary layers of past games states)

--- Gopher/PolicyNet.cpp
+++ Gopher/PolicyNet.cpp
@@ -1,16 +1,10 @@
 #include "PolicyNet.h"
 #include "defines.h"
+#include <cntk\Include\CNTKLibrary.h>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
 
-#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
-#define COMPILER_MSVC
-#define NOMINMAX
-#include <tensorflow\core\public\session.h>
-#include <tensorflow\core\protobuf\meta_graph.pb.h>
-#include <tensorflow\core\framework\tensor.h>
-#include <tensorflow\cc\ops\standard_ops.h>
-//#include <tensorflow\cc\client\client_session.h>
-
-namespace tf = tensorflow;
 
 // Move these to defines once relevent for storage
 static constexpr int LayersPerState = 2;
@@ -18,102 +12,33 @@ static constexpr int BoardHistory = 3;
 static constexpr int BoardDepth = LayersPerState * BoardHistory + 1;
 static constexpr int InputSize = BoardDepth * BoardSize2;
 
-static const std::string pathToCheckpoint = "Models/PolicyModel/latestModel";
-static const std::string pathToGraph = pathToCheckpoint + ".meta";
-
-//static const std::string pathToGraph = "Models/PolicyModel/graph.pb";
-
-#include <iomanip>
-#include <tensorflow\cc\saved_model\loader.h>
+static const std::string modelPath = "models/PolicyModel/";
+static const std::string fileName = "GoNet.dnn";
 
 namespace PolicyNet
 {
-	tf::Status status;
-	tf::Session* session;
-	tf::SessionOptions options;
-	tf::MetaGraphDef graphDef;
+	CNTK::FunctionPtr		model;
+	CNTK::DeviceDescriptor*  device;
+	CNTK::Variable		    inputVar;
+	CNTK::Variable			outputVar;
 
-// TODO: Make the net optional
 void init()
 {
-	// Set memory growth manually. Resolves and issue with tf 1.5 for windows
-	double memFraction = 1.0;
-	bool allowMemGrowth = true;
-	options.config.mutable_gpu_options()->set_allow_growth(allowMemGrowth);
-	options.config.mutable_gpu_options()->set_per_process_gpu_memory_fraction(memFraction);
+	std::ifstream ifs(modelPath + fileName, std::ifstream::in | std::ifstream::binary);
 
-	session = tf::NewSession(options);
-	if (!session)
-		throw std::runtime_error("Could no create Tensorflow Session!");
+	model     = CNTK::Function::Load(ifs);
+	device    = &CNTK::DeviceDescriptor::UseDefaultDevice();
+	inputVar  = model->Arguments()[0];
+	outputVar = model->Output();
 
-	// Read in the protobuf
-	status = tf::ReadBinaryProto(tf::Env::Default(), pathToGraph, &graphDef);
-	if (!status.ok())
-		throw std::runtime_error("Error reading graph: " + status.ToString());
+	// Just for testing
+	auto shp = inputVar.Shape();
 
-	status = session->Create(graphDef.graph_def());
-	if (!status.ok())
-		throw std::runtime_error("Error creating graph: " + status.ToString());
-
-	// Read the weights
-	tf::Tensor checkpointPathTensor(tf::DT_STRING, tf::TensorShape());
-	checkpointPathTensor.scalar<std::string>()() = pathToCheckpoint;
-	
-	status = session->Run(
-		{ { graphDef.saver_def().filename_tensor_name(), checkpointPathTensor }, },
-		{},
-		{ graphDef.saver_def().restore_op_name() },
-		nullptr
-	);
-	
-	if (!status.ok())
-		throw std::runtime_error("Error loading checkpoint from " + pathToCheckpoint + ": " + status.ToString());
-
-	run();
 }
 
 void run()
 {
-	
-	static const std::string inputName = "Input_input"; 
-	static const std::string outputName = "Output/Softmax:0";
 
-	static const auto shape = tf::TensorShape({ 1, BoardDepth, BoardSize, BoardSize });
-
-	// Placeholder until we're using actual input data
-	float inData[InputSize] = { 0.f };
-
-	tf::Tensor input(tf::DT_FLOAT, shape);
-	std::copy_n(inData, InputSize, input.flat<float>().data());
-
-	std::vector<tf::Tensor> outputs;
-	status = session->Run({ { inputName, input } }, { outputName }, {}, &outputs);
-
-	tf::TTypes<float>::Flat flatOut = outputs[0].flat<float>();
-
-	for (int i = 0; i < BoardSize2; ++i)
-	{
-		if (i % BoardSize == 0)
-			std::cout << '\n';
-		std::cout << std::setw(8) << std::fixed << std::setprecision(5) << flatOut(i) * 100.0 << ", ";
-	}
-
-	int a = 5;
-	/*
-	// and run the inference to your liking
-	auto feedDict = ...
-	auto outputOps = ...
-	status = session->Run(feedDict, outputOps, {}, &outputTensors);
-
-	*/
 }
-
-
-
-
-
-
-
-
 
 }
