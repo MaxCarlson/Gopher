45b089f8eda574659b456964f2cdf934a41eb7ef
Maxwell Carlson <bart4282819@gmail.com>
2018-07-02 13:56:26 -0700

    Added a small demo of the net, showing it working (when given an input of all zeros (which represents black to move turn 0) it outputs areas where you would expect it to want to play as higher rated areas

--- Gopher/PolicyNet.cpp
+++ Gopher/PolicyNet.cpp
@@ -34,11 +34,56 @@ void init()
 	// Just for testing
 	auto shp = inputVar.Shape();
 
+	// TODO: Now we need to 
+	// 1: Figure out how we want to store the binary board states
+	// 2: Implement the storing or creation of the needed inputs
+	// 3: Figure out what orientation the flat inputVar takes (compared to the models 3D input)
+
+	run();
+}
+
+template<class T>
+void printNetOut(size_t size, const std::vector<std::vector<T>>& outputBuffer)
+{
+	std::cout << '\n';
+	auto out = outputBuffer[0];
+	for (auto i = 0; i < out.size(); ++i)
+	{
+		if (i % 19 == 0 && i != 0)
+			std::cout << '\n';
+
+		std::cout << std::fixed << std::setprecision(4)
+			<< std::setw(7) << out[i] << ", ";
+	}
+	std::cout << '\n';
+}
+
+void testRun()
+{
+	std::vector<float> inputData(inputVar.Shape().TotalSize());
+	std::fill(inputData.begin(), inputData.end(), 0.f);
+
+	// Create input value and input data map
+	auto inputVal = CNTK::Value::CreateBatch(inputVar.Shape(), inputData, *device);
+	std::unordered_map<CNTK::Variable, CNTK::ValuePtr> inputDataMap = { { inputVar, inputVal } };
+
+	// Create output data map. Using null as Value to indicate using system allocated memory.
+	// Alternatively, create a Value object and add it to the data map.
+	std::unordered_map<CNTK::Variable, CNTK::ValuePtr> outputDataMap = { { outputVar, nullptr } };
+
+	// Evaluate!
+	model->Evaluate(inputDataMap, outputDataMap, *device);
+
+	auto outputVal = outputDataMap[outputVar];
+	std::vector<std::vector<float>> outputData;
+	outputVal->CopyVariableValueTo(outputVar, outputData);
+
+	printNetOut(outputVar.Shape().TotalSize(), outputData);
 }
 
 void run()
 {
-
+	testRun();
 }
 
 }
