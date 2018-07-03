6ffa061cfe36c54dbfadc04a188f6a556c9b040f
Maxwell Carlson <bart4282819@gmail.com>
2018-07-02 21:00:10 -0700

    Code for converting current and previous game states is written, but not bug tested yet.

--- Gopher/GameState.cpp
+++ Gopher/GameState.cpp
@@ -23,14 +23,35 @@ void GameState::clear()
 	stateIdx = 0;
 }
 
+inline int getPaddedIdx(int x, int y)
+{
+	return (y + 1) * BoardRealSize + (x + 1);
+}
+
 NetInput GameState::genNetInput(int color) const
 {
 	NetInput input;
 
+	// Fill the color slice with appropriate color
 	std::fill_n(input.slices[0], BoardSize2, static_cast<float>(color - 1));
 	
+	// Fill the rest of the binary slices
+	int sliceIdx = 1;
 	for (int i = 0; (i < moves && i < BoardHistory); ++i)
 	{
-
+		for (int c = 0; c < 2; ++c)
+		{
+			++sliceIdx;
+			int idx = 0;
+			for (int x = 0; x < BoardSize; ++x)
+				for (int y = 0; y < BoardSize; ++y)
+				{
+					const int pIdx = getPaddedIdx(x, y);
+					input.slices[sliceIdx][idx] = static_cast<float>(states[stateIdx - sliceIdx][pIdx]);
+					++idx;
+				}
+		}
 	}
+
+	return input;
 }
